f02372d97dd47d4a814fedeb1ee5216e54adf814
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-09 13:58:01 +0100

    entrying players

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,13 @@
+
+feature 'Enter names' do
+    scenario 'submitting names' do
+        visit('/')
+        fill_in :player_1_name, with: 'Boris'
+        fill_in :player_2_name, with: 'Doris'
+        click_button 'Submit'
+
+        save_and_open_page
+        
+        expect(page).to have_content 'Boris vs. Doris'
+    end
+end
