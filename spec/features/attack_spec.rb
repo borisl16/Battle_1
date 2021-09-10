cbd7b8af952185bbb0c99dfdbea6aef4600636be
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-10 10:59:08 +0100

    entrying player 2

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,10 @@
 feature 'Attacking' do
     scenario 'attack Player 2' do
       sign_in_and_play
-      save_and_open_page
-      click_button 'Attack'
+      click_link 'Attack'
       expect(page).to have_content 'Charlotte attacked Mittens'
     end
   end
+
+    
   
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,8 @@
 
 feature 'Enter names' do
     scenario 'submitting names' do
-        sign_in_and_play
-        save_and_open_page
-      
-      expect(page).to have_content 'Charlotte vs. Mittens'
+        sign_in_and_play 
+        expect(page).to have_content 'Charlotte vs. Mittens'
     end
 end
   
