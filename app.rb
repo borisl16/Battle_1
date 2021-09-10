40825df81e81d935ce3406a5ad25efd35ec72e29
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-10 14:30:13 +0100

    implementing hit points

--- app.rb
+++ app.rb
@@ -4,32 +4,36 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'capybara'
 
+
 class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
     enable :sessions
 
   get '/' do
     erb :index
   end
 
-  get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
-    erb :attack
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
   end
- 
-post '/names' do
-  session[:player_1_name] = params[:player_1_name]
-  session[:player_2_name] = params[:player_2_name]
-  redirect '/play'
+
+get '/play' do
+  @player_1_name = $player_1.name
+  @player_2_name = $player_2.name
+  erb :play
 end
 
-
-  get '/play' do
-   @player_1_name = session[:player_1_name]
-   @player_2_name = session[:player_2_name]
-   erb :play
-  end
-
+get '/attack' do
+  @player_1 = $player_1
+  @player_2 = $player_2
+  Game.new.attack(@player_2)
+  erb :attack
+end
 
   run! if app_file == $0
 end
